a73a97b5578960484ff8a7eb43f121bdc888966c
Sherine Awad <sherinem@gl3079.arc-ts.umich.edu>
2026-02-12 10:19:10 -0500

    add more samples and update plots

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ### Samples 
 
-- NL16 
-- NL17 
-- NL18 
+| sample_name | atac_file                  | rna_file                           |
+| ----------- | -------------------------- | ---------------------------------- |
+| NL13        | NL13_atac_fragments.tsv.gz | NL13_filtered_feature_bc_matrix.h5 |
+| NL14        | NL14_atac_fragments.tsv.gz | NL14_filtered_feature_bc_matrix.h5 |
+| NL15        | NL15_atac_fragments.tsv.gz | NL15_filtered_feature_bc_matrix.h5 |
+| NL16        | NL16_atac_fragments.tsv.gz | NL16_filtered_feature_bc_matrix.h5 |
+| NL17        | NL17_atac_fragments.tsv.gz | NL17_filtered_feature_bc_matrix.h5 |
+| NL18        | NL18_atac_fragments.tsv.gz | NL18_filtered_feature_bc_matrix.h5 |
+
 
 ### Filtering Thresholds
 
@@ -18,77 +24,110 @@ We used the following filtering criteria:
 * **Maximum number of UMIs in RNA-seq**: Filters out cells with abnormally high counts that may indicate doublets. Default is **30000**.
 
 
-[pre filter](figures/NFI_preFilterQC.pdf)
+## Initial Cell Counts
 
-[post filter](figures/NFI_postFilterQC.pdf)
+- **Total cells before filtering:** 57,943
+
+### Cells per sample
+
+| Sample | Cells |
+|--------|-------|
+| NL13   | 11,641 |
+| NL14   | 14,436 |
+| NL15   | 14,611 |
+| NL16   | 3,784  |
+| NL17   | 6,363  |
+| NL18   | 7,108  |
+
+---
+
+## Post-filtering Cell Counts
+
+- **Total cells after filtering:** 41,616
+
+### Cells per sample
+
+| Sample | Cells |
+|--------|-------|
+| NL13   | 8,844  |
+| NL14   | 10,476 |
+| NL15   | 11,506 |
+| NL16   | 2,635  |
+| NL17   | 3,809  |
+| NL18   | 4,346  |
+
+
+
+[click here to view pre filter report](figures/NFI_preFilterQC.pdf)
+
+[click here to view post filter report](figures/NFI_postFilterQC.pdf)
+
 
 
 ## UMAPs
 
 #### RNA 
-![](figures/NFI_filtered_UMAP_RNA_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_RNA_UMAP.png?v=3)
 #### ATAC
-![](figures/NFI_filtered_UMAP_ATAC_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_ATAC_UMAP.png?v=3)
 #### Combined 
-![](figures/NFI_filtered_UMAP_Combined_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_Combined_UMAP.png?v=3)
 
 ## QCs 
 
-#### RNA 
-![](figures/NFI_filtered_UMAP_Clusters_RNA_QC.png?v=2)
 #### ATAC 
-![](figures/NFI_filtered_UMAP_Clusters_ATAC_QC.png?v=2)
+![](figures/NFI_filtered_UMAP_Clusters_ATAC_QC.png?v=3)
 #### Combined 
-![](figures/NFI_filtered_UMAP_Clusters_Combined_QC.png?v=2)
+![](figures/NFI_filtered_UMAP_Clusters_Combined_QC.png?v=3)
 
 
 ## Per sample UMAP 
 
 #### RNA
-![](figures/NFI_filtered_UMAP_RNA_SAMPLE_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_RNA_SAMPLE_UMAP.png?v=3)
 #### ATAC
-![](figures/NFI_filtered_UMAP_ATAC_SAMPLE_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_ATAC_SAMPLE_UMAP.png?v=3)
 #### Combined
-![](figures/NFI_filtered_UMAP_Combined_SAMPLE_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_Combined_SAMPLE_UMAP.png?v=3)
 
 
 ## Clusters 
 
-![](figures/NFI_filtered_UMAP_Combined_SAMPLES_GRID.png?v=2)
+![](figures/NFI_filtered_UMAP_Combined_SAMPLES_GRID.png?v=3)
 
 #### Combined 
 
-![](figures/NFI_filtered_UMAP_BothSamples_CombinedClusters_UMAP.png?v=2)
+![](figures/NFI_filtered_UMAP_BothSamples_CombinedClusters_UMAP.png?v=3)
 
 ## Marker genes 
 
-<img src="figures/NFI_filtered_UMAP_Pax6_UMAP.png?v=2" alt="pax6" width="33%"><img src="figures/NFI_filtered_UMAP_Rlbp1_UMAP.png?v=2" alt="rlbp1" width="33%"><img src="figures/NFI_filtered_UMAP_Prdx6_UMAP.png?v=2" alt="prdx6" width="33%">
+<img src="figures/NFI_filtered_UMAP_Pax6_UMAP.png?v=3" alt="pax6" width="33%"><img src="figures/NFI_filtered_UMAP_Rlbp1_UMAP.png?v=3" alt="rlbp1" width="33%"><img src="figures/NFI_filtered_UMAP_Prdx6_UMAP.png?v=3" alt="prdx6" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Abca8a_UMAP.png?v=2" alt="abca8a" width="33%"><img src="figures/NFI_filtered_UMAP_Notch1_UMAP.png?v=2" alt="notch1" width="33%"><img src="figures/NFI_filtered_UMAP_Hes1_UMAP.png?v=2" alt="hes1" width="33%">
+<img src="figures/NFI_filtered_UMAP_Abca8a_UMAP.png?v=3" alt="abca8a" width="33%"><img src="figures/NFI_filtered_UMAP_Notch1_UMAP.png?v=3" alt="notch1" width="33%"><img src="figures/NFI_filtered_UMAP_Hes1_UMAP.png?v=3" alt="hes1" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Vim_UMAP.png?v=2" alt="vim" width="33%"><img src="figures/NFI_filtered_UMAP_Lhx2_UMAP.png?v=2" alt="lhx2" width="33%"><img src="figures/NFI_filtered_UMAP_Gfap_UMAP.png?v=2" alt="gfap" width="33%">
+<img src="figures/NFI_filtered_UMAP_Vim_UMAP.png?v=3" alt="vim" width="33%"><img src="figures/NFI_filtered_UMAP_Lhx2_UMAP.png?v=3" alt="lhx2" width="33%"><img src="figures/NFI_filtered_UMAP_Gfap_UMAP.png?v=3" alt="gfap" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Sox11_UMAP.png?v=2" alt="sox11" width="33%"><img src="figures/NFI_filtered_UMAP_Prdm1_UMAP.png?v=2" alt="prdm1" width="33%"><img src="figures/NFI_filtered_UMAP_Rho_UMAP.png?v=2" alt="rho" width="33%">
+<img src="figures/NFI_filtered_UMAP_Sox11_UMAP.png?v=3" alt="sox11" width="33%"><img src="figures/NFI_filtered_UMAP_Prdm1_UMAP.png?v=3" alt="prdm1" width="33%"><img src="figures/NFI_filtered_UMAP_Rho_UMAP.png?v=3" alt="rho" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Otx2_UMAP.png?v=2" alt="otx2" width="33%"><img src="figures/NFI_filtered_UMAP_Insm1_UMAP.png?v=2" alt="insm1" width="33%"><img src="figures/NFI_filtered_UMAP_Bsn_UMAP.png?v=2" alt="bsn" width="33%">
+<img src="figures/NFI_filtered_UMAP_Otx2_UMAP.png?v=3" alt="otx2" width="33%"><img src="figures/NFI_filtered_UMAP_Insm1_UMAP.png?v=3" alt="insm1" width="33%"><img src="figures/NFI_filtered_UMAP_Bsn_UMAP.png?v=3" alt="bsn" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Slc6a9_UMAP.png?v=2" alt="slc6a9" width="33%"><img src="figures/NFI_filtered_UMAP_Slc17a7_UMAP.png?v=2" alt="slc17a7" width="33%"><img src="figures/NFI_filtered_UMAP_Nrl_UMAP.png?v=2" alt="nrl" width="33%">
+<img src="figures/NFI_filtered_UMAP_Slc6a9_UMAP.png?v=3" alt="slc6a9" width="33%"><img src="figures/NFI_filtered_UMAP_Slc17a7_UMAP.png?v=3" alt="slc17a7" width="33%"><img src="figures/NFI_filtered_UMAP_Nrl_UMAP.png?v=3" alt="nrl" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Cabp5_UMAP.png?v=2" alt="cabp5" width="33%"><img src="figures/NFI_filtered_UMAP_Lhx4_UMAP.png?v=2" alt="lhx4" width="33%"><img src="figures/NFI_filtered_UMAP_Rbfox3_UMAP.png?v=2" alt="rbfox3" width="33%">
+<img src="figures/NFI_filtered_UMAP_Cabp5_UMAP.png?v=3" alt="cabp5" width="33%"><img src="figures/NFI_filtered_UMAP_Lhx4_UMAP.png?v=3" alt="lhx4" width="33%"><img src="figures/NFI_filtered_UMAP_Rbfox3_UMAP.png?v=3" alt="rbfox3" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Pax2_UMAP.png?v=2" alt="pax2" width="33%"><img src="figures/NFI_filtered_UMAP_Tfap2a_UMAP.png?v=2" alt="tfap2a" width="33%"><img src="figures/NFI_filtered_UMAP_Calb1_UMAP.png?v=2" alt="calb1" width="33%">
+<img src="figures/NFI_filtered_UMAP_Pax2_UMAP.png?v=3" alt="pax2" width="33%"><img src="figures/NFI_filtered_UMAP_Tfap2a_UMAP.png?v=3" alt="tfap2a" width="33%"><img src="figures/NFI_filtered_UMAP_Calb1_UMAP.png?v=3" alt="calb1" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Calb2_UMAP.png?v=2" alt="calb2" width="33%"><img src="figures/NFI_filtered_UMAP_Gad1_UMAP.png?v=2" alt="gad1" width="33%"><img src="figures/NFI_filtered_UMAP_Csf1r_UMAP.png?v=2" alt="csf1r" width="33%">
+<img src="figures/NFI_filtered_UMAP_Calb2_UMAP.png?v=3" alt="calb2" width="33%"><img src="figures/NFI_filtered_UMAP_Gad1_UMAP.png?v=3" alt="gad1" width="33%"><img src="figures/NFI_filtered_UMAP_Csf1r_UMAP.png?v=3" alt="csf1r" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Pou4f2_UMAP.png?v=2" alt="pou4f2" width="33%"><img src="figures/NFI_filtered_UMAP_Ccr2_UMAP.png?v=2" alt="ccr2" width="33%"><img src="figures/NFI_filtered_UMAP_Isl1_UMAP.png?v=2" alt="isl1" width="33%">
+<img src="figures/NFI_filtered_UMAP_Pou4f2_UMAP.png?v=3" alt="pou4f2" width="33%"><img src="figures/NFI_filtered_UMAP_Ccr2_UMAP.png?v=3" alt="ccr2" width="33%"><img src="figures/NFI_filtered_UMAP_Isl1_UMAP.png?v=3" alt="isl1" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Lhx1_UMAP.png?v=2" alt="lhx1" width="33%"><img src="figures/NFI_filtered_UMAP_Arr3_UMAP.png?v=2" alt="arr3" width="33%"><img src="figures/NFI_filtered_UMAP_Acta2_UMAP.png?v=2" alt="acta2" width="33%">
+<img src="figures/NFI_filtered_UMAP_Lhx1_UMAP.png?v=3" alt="lhx1" width="33%"><img src="figures/NFI_filtered_UMAP_Arr3_UMAP.png?v=3" alt="arr3" width="33%"><img src="figures/NFI_filtered_UMAP_Acta2_UMAP.png?v=3" alt="acta2" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Sebox_UMAP.png?v=2" alt="sebox" width="33%"><img src="figures/NFI_filtered_UMAP_Kcnj8_UMAP.png?v=2" alt="kcnj8" width="33%"><img src="figures/NFI_filtered_UMAP_Atoh7_UMAP.png?v=2" alt="atoh7" width="33%">
+<img src="figures/NFI_filtered_UMAP_Sebox_UMAP.png?v=3" alt="sebox" width="33%"><img src="figures/NFI_filtered_UMAP_Kcnj8_UMAP.png?v=3" alt="kcnj8" width="33%"><img src="figures/NFI_filtered_UMAP_Atoh7_UMAP.png?v=3" alt="atoh7" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Emx1_UMAP.png?v=2" alt="emx1" width="33%"><img src="figures/NFI_filtered_UMAP_Rpe65_UMAP.png?v=2" alt="rpe65" width="33%"><img src="figures/NFI_filtered_UMAP_Foxn4_UMAP.png?v=2" alt="foxn4" width="33%">
+<img src="figures/NFI_filtered_UMAP_Emx1_UMAP.png?v=3" alt="emx1" width="33%"><img src="figures/NFI_filtered_UMAP_Rpe65_UMAP.png?v=3" alt="rpe65" width="33%"><img src="figures/NFI_filtered_UMAP_Foxn4_UMAP.png?v=3" alt="foxn4" width="33%">
 
-<img src="figures/NFI_filtered_UMAP_Chat_UMAP.png?v=2" alt="chat" width="33%"><img src="figures/NFI_filtered_UMAP_Tie1_UMAP.png?v=2" alt="tie1" width="33%">
+<img src="figures/NFI_filtered_UMAP_Chat_UMAP.png?v=3" alt="chat" width="33%"><img src="figures/NFI_filtered_UMAP_Tie1_UMAP.png?v=3" alt="tie1" width="33%">
 
 
 
---
+++ src/plot.R
@@ -0,0 +1,298 @@
+library(ArchR)
+library(ggplot2)
+library(argparse)
+library(gridExtra)
+library(patchwork)
+
+# Parser setup
+parser <- ArgumentParser(description = 'Create UMAP plots from existing ArchR project')
+parser$add_argument('--project_name', type = 'character', required = TRUE)
+parser$add_argument('--suffix', type = 'character', default = '')
+args <- parser$parse_args()
+
+# Suppress Rplots.pdf creation
+pdf(NULL)
+
+# Load existing project from current directory
+proj_ALL <- loadArchRProject(path = args$project_name, force = FALSE, showLogo = TRUE)
+
+cat(sprintf("Loaded project with %d cells\n", nCells(proj_ALL)))
+
+# Helper function to check if embedding exists
+embedding_exists <- function(proj, embedding_name) {
+  tryCatch({
+    emb <- getEmbedding(ArchRProj = proj, embedding = embedding_name)
+    return(TRUE)
+  }, error = function(e) {
+    return(FALSE)
+  })
+}
+
+# Get cell metadata
+cell_col_data <- getCellColData(proj_ALL)
+
+# ORIGINAL PLOTS - EXACT SAME AS BEFORE
+p1 <- plotEmbedding(
+  ArchRProj = proj_ALL,
+  colorBy = "cellColData",
+  name = "Clusters_RNA",
+  embedding = "UMAP_RNA",
+  plotAs = "points",
+  size = 0.5
+)
+
+p2 <- plotEmbedding(
+  ArchRProj = proj_ALL,
+  colorBy = "cellColData",
+  name = "Clusters_ATAC",
+  embedding = "UMAP_ATAC",
+  plotAs = "points",
+  size = 0.5
+)
+
+p3 <- plotEmbedding(
+  ArchRProj = proj_ALL,
+  colorBy = "cellColData",
+  name = "Clusters_Combined",
+  embedding = "UMAP_Combined",
+  plotAs = "points",
+  size = 0.5
+)
+
+# Save ORIGINAL plots with ORIGINAL names
+base_name <- basename(args$project_name)
+pdf(paste0(base_name, args$suffix, "_RNA_UMAP.pdf"), width = 12, height = 8)
+print(p1)
+dev.off()
+
+pdf(paste0(base_name, args$suffix, "_ATAC_UMAP.pdf"), width = 12, height = 8)
+print(p2)
+dev.off()
+
+pdf(paste0(base_name, args$suffix, "_Combined_UMAP.pdf"), width = 12, height = 8)
+print(p3)
+dev.off()
+
+cat("Created original 3 UMAP plots\n")
+
+# 1. UMAP per sample (with NEW file names)
+if("Sample" %in% colnames(cell_col_data)) {
+  cat("Creating UMAP plots per sample...\n")
+  
+  # RNA UMAP colored by sample
+  if(embedding_exists(proj_ALL, "UMAP_RNA")) {
+    p_sample_rna <- plotEmbedding(
+      ArchRProj = proj_ALL,
+      colorBy = "cellColData",
+      name = "Sample",
+      embedding = "UMAP_RNA",
+      plotAs = "points",
+      size = 0.5
+    )
+    
+    pdf(paste0(base_name, args$suffix, "_RNA_SAMPLE_UMAP.pdf"), width = 12, height = 8)
+    print(p_sample_rna)
+    dev.off()
+    cat("  Created RNA_SAMPLE_UMAP.pdf\n")
+  }
+  
+  # ATAC UMAP colored by sample
+  if(embedding_exists(proj_ALL, "UMAP_ATAC")) {
+    p_sample_atac <- plotEmbedding(
+      ArchRProj = proj_ALL,
+      colorBy = "cellColData",
+      name = "Sample",
+      embedding = "UMAP_ATAC",
+      plotAs = "points",
+      size = 0.5
+    )
+    
+    pdf(paste0(base_name, args$suffix, "_ATAC_SAMPLE_UMAP.pdf"), width = 12, height = 8)
+    print(p_sample_atac)
+    dev.off()
+    cat("  Created ATAC_SAMPLE_UMAP.pdf\n")
+  }
+  
+  # Combined UMAP colored by sample
+  if(embedding_exists(proj_ALL, "UMAP_Combined")) {
+    p_sample_combined <- plotEmbedding(
+      ArchRProj = proj_ALL,
+      colorBy = "cellColData",
+      name = "Sample",
+      embedding = "UMAP_Combined",
+      plotAs = "points",
+      size = 0.5
+    )
+    
+    pdf(paste0(base_name, args$suffix, "_Combined_SAMPLE_UMAP.pdf"), width = 12, height = 8)
+    print(p_sample_combined)
+    dev.off()
+    cat("  Created Combined_SAMPLE_UMAP.pdf\n")
+  }
+}
+
+# 2. UMAP per sample for COMBINED embedding only
+if("Sample" %in% colnames(cell_col_data) && embedding_exists(proj_ALL, "UMAP_Combined")) {
+  cat("Creating UMAP per sample for Combined embedding...\n")
+  
+  samples <- unique(cell_col_data$Sample)
+  sample_plots <- list()
+  
+  for(sample in samples) {
+    sample_cells <- which(cell_col_data$Sample == sample)
+    proj_sample <- proj_ALL[sample_cells, ]
+    
+    p <- plotEmbedding(
+      ArchRProj = proj_sample,
+      colorBy = "cellColData",
+      name = "Clusters_Combined",
+      embedding = "UMAP_Combined",
+      plotAs = "points",
+      size = 1.0
+    ) + ggtitle(sample)
+    
+    sample_plots[[sample]] <- p
+  }
+  
+  n_cols <- min(3, length(samples))
+  n_rows <- ceiling(length(samples) / n_cols)
+  
+  combined_plot <- wrap_plots(sample_plots, ncol = n_cols, nrow = n_rows)
+  
+  pdf(paste0(base_name, args$suffix, "_Combined_SAMPLES_GRID.pdf"),
+      width = 6 * n_cols, height = 5 * n_rows)
+  print(combined_plot)
+  dev.off()
+  
+  cat("  Created Combined_SAMPLES_GRID.pdf\n")
+}
+
+# 3. BOTH SAMPLES TOGETHER with clusters for annotation
+if("Sample" %in% colnames(cell_col_data) && embedding_exists(proj_ALL, "UMAP_Combined")) {
+  cat("Creating both samples together with clusters...\n")
+  
+  # Create plot with both samples, colored by clusters
+  p_both_samples <- plotEmbedding(
+    ArchRProj = proj_ALL,
+    colorBy = "cellColData",
+    name = "Clusters_Combined",
+    embedding = "UMAP_Combined",
+    plotAs = "points",
+    size = 0.5
+  )
+  
+  pdf(paste0(base_name, args$suffix, "_BothSamples_CombinedClusters_UMAP.pdf"), width = 12, height = 8)
+  print(p_both_samples)
+  dev.off()
+  
+  cat("  Created BothSamples_CombinedClusters_UMAP.pdf\n")
+}
+
+# 4. CORRECTED: QC plots per clusters with proper assay-specific metrics
+cat("Creating QC plots per clusters with proper assay-specific metrics...\n")
+
+# CORRECT: TSSEnrichment is ATAC-seq specific, RNA-seq has no equivalent in ArchR
+qc_metrics_atac <- c("TSSEnrichment", "nFrags", "BlacklistRatio", "NucleosomeRatio")
+# RNA-seq has no standard QC metrics stored in cellColData (these are all ATAC metrics)
+
+# Only keep metrics that exist in the project
+qc_metrics_atac <- qc_metrics_atac[qc_metrics_atac %in% colnames(cell_col_data)]
+
+# For Combined clusters - show ATAC QC metrics only (no RNA QC metrics exist)
+if("Clusters_Combined" %in% colnames(cell_col_data) && length(qc_metrics_atac) > 0) {
+  qc_plot_list <- list()
+  
+  # Add ATAC-specific QC plots (including TSSEnrichment)
+  for(qc_metric in qc_metrics_atac) {
+    df <- data.frame(
+      Cluster = cell_col_data[["Clusters_Combined"]],
+      Value = cell_col_data[[qc_metric]]
+    )
+    df <- df[complete.cases(df), ]
+    
+    if(nrow(df) > 0) {
+      p <- ggplot(df, aes(x = Cluster, y = Value, fill = Cluster)) +
+        geom_violin(scale = "width", trim = TRUE) +
+        geom_boxplot(width = 0.1, fill = "white", outlier.shape = NA) +
+        theme_classic() +
+        theme(
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = "none"
+        ) +
+        labs(title = paste(qc_metric, "(ATAC) by Combined Clusters"), 
+             y = qc_metric, x = "Cluster")
+      qc_plot_list[[qc_metric]] <- p
+    }
+  }
+  
+  if(length(qc_plot_list) > 0) {
+    n_cols <- min(2, length(qc_plot_list))
+    n_rows <- ceiling(length(qc_plot_list) / n_cols)
+    
+    combined_qc_plot <- wrap_plots(qc_plot_list, ncol = n_cols, nrow = n_rows)
+    
+    pdf(paste0(base_name, args$suffix, "_Clusters_Combined_QC.pdf"),
+        width = 8 * n_cols, height = 6 * n_rows)
+    print(combined_qc_plot)
+    dev.off()
+    cat("  Created Clusters_Combined_QC.pdf with ATAC metrics (TSSEnrichment, nFrags, BlacklistRatio, NucleosomeRatio)\n")
+  }
+}
+
+# For RNA clusters - NO QC METRICS (RNA-seq QC not stored in cellColData)
+if("Clusters_RNA" %in% colnames(cell_col_data)) {
+  cat("  NOTE: No RNA-seq QC metrics available in cellColData - skipping QC plots for RNA clusters\n")
+}
+
+# For ATAC clusters - only ATAC-specific QC metrics
+if("Clusters_ATAC" %in% colnames(cell_col_data) && length(qc_metrics_atac) > 0) {
+  qc_plot_list <- list()
+  
+  for(qc_metric in qc_metrics_atac) {
+    df <- data.frame(
+      Cluster = cell_col_data[["Clusters_ATAC"]],
+      Value = cell_col_data[[qc_metric]]
+    )
+    df <- df[complete.cases(df), ]
+    
+    if(nrow(df) > 0) {
+      p <- ggplot(df, aes(x = Cluster, y = Value, fill = Cluster)) +
+        geom_violin(scale = "width", trim = TRUE) +
+        geom_boxplot(width = 0.1, fill = "white", outlier.shape = NA) +
+        theme_classic() +
+        theme(
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = "none"
+        ) +
+        labs(title = paste(qc_metric, "by ATAC Clusters"),
+             y = qc_metric, x = "Cluster")
+      qc_plot_list[[qc_metric]] <- p
+    }
+  }
+  
+  if(length(qc_plot_list) > 0) {
+    n_cols <- min(2, length(qc_plot_list))
+    n_rows <- ceiling(length(qc_plot_list) / n_cols)
+    
+    combined_qc_plot <- wrap_plots(qc_plot_list, ncol = n_cols, nrow = n_rows)
+    
+    pdf(paste0(base_name, args$suffix, "_Clusters_ATAC_QC.pdf"),
+        width = 8 * n_cols, height = 6 * n_rows)
+    print(combined_qc_plot)
+    dev.off()
+    cat("  Created Clusters_ATAC_QC.pdf with ATAC metrics (TSSEnrichment, nFrags, BlacklistRatio, NucleosomeRatio)\n")
+  }
+}
+
+cat("=== ALL PLOTS CREATED SUCCESSFULLY ===\n")
+cat("Original files (unchanged):\n")
+cat("  ", base_name, args$suffix, "_RNA_UMAP.pdf\n", sep="")
+cat("  ", base_name, args$suffix, "_ATAC_UMAP.pdf\n", sep="")
+cat("  ", base_name, args$suffix, "_Combined_UMAP.pdf\n", sep="")
+cat("\nNew files added:\n")
+cat("  *_SAMPLE_UMAP.pdf (colored by sample)\n")
+cat("  Combined_SAMPLES_GRID.pdf (each sample separately - Combined only)\n")
+cat("  BothSamples_CombinedClusters_UMAP.pdf (both samples with clusters)\n")
+cat("  Clusters_Combined_QC.pdf (ATAC metrics: TSSEnrichment, nFrags, BlacklistRatio, NucleosomeRatio)\n")
+cat("  Clusters_ATAC_QC.pdf (ATAC metrics: TSSEnrichment, nFrags, BlacklistRatio, NucleosomeRatio)\n")
+cat("  Clusters_RNA_QC.pdf - NOT CREATED (no RNA QC metrics in cellColData)\n")
